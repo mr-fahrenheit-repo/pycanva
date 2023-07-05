fe83d1dc25b326c99725ee5c1ed1260d1e590821
otong <work@rifkyashari.com>
2023-07-05 17:25:27 +0700

    Merging

--- install.bat
+++ install.bat
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 @echo off
 echo pip install -r requirements.txt
 pip install -r requirements.txt
 echo install dependecies successful
 pause
-=======
-@echo off
-echo pip install -r requirements.txt
-pip install -r requirements.txt
-echo install dependecies successful
-pause
->>>>>>> 2be5804e35e4a30db48e2e5d63a435c374d88dfe
--- run.bat
+++ run.bat
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 @echo off
 python main.py
 pause
-=======
-@echo off
-python main.py
-pause
->>>>>>> 2be5804e35e4a30db48e2e5d63a435c374d88dfe
