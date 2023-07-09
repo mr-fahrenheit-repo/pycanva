643a6b8c769c16d1ec9e320bc6a2f028cf0b0848
otong <work@rifkyashari.com>
2023-07-10 00:15:05 +0700

    add name list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import Libraries
 import pyautogui
+pyautogui.FAILSAFE = False
 import time
 import random
 import os
@@ -122,8 +123,11 @@ def username():
 def vpn(code):
     os.system('"ipconfig /flushdns"')
     os.system(f'"mullvad relay set hostname {code}"')
-    os.system('"mullvad connect"') 
-    
+    os.system('"mullvad connect"')
+    hold(10,12)
+    os.system('"mullvad status"')
+    print("===================================================")
+     
 # Locate element by x,y coordinate and RGB value
 def locate_element(x,y,r,g,b):
     pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
@@ -132,6 +136,19 @@ def locate_element(x,y,r,g,b):
         if param == True:
             pyautogui.click()
             break
+
+# Locate element by x,y coordinate and RGB value with timeout 10
+def locate_element_10(x,y,r,g,b):
+    pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
+    for x in range(5):
+        param = pyautogui.pixelMatchesColor(x, y, (r, g, b))
+        if param == True:
+            pyautogui.click()
+            break
+        else:
+            pyautogui.click()
+            time.sleep(2)
+            pass
 
 # Locate element by x,y coordinate and RGB value with y axis adjustment     
 def y_locate_element(x,y,r,g,b):
@@ -157,55 +174,73 @@ def hold(start, stop):
 
 # Opening up Chrome browser
 def openchrome():
+    print("1. Opening chrome browser")
     pyautogui.press('win')
     hold(0.75,1)
     pyautogui.write("chrome", interval = 0.12)
     hold(0.75,1)
     pyautogui.press("enter")
+    hold(10,15)
 
 # Open the target URL
 def opencanva():
+    print("2. Opening Canva")
     pyautogui.write(url, interval=0.25)
     hold(0.35,0.5)
     pyautogui.press("enter")
     hold(2,3)
     pyautogui.press("f11")
+    hold(5,7)
     
 # Locate and click Accept Cookies prompt         
 def allowcookies():
-    locate_element(381,1009,173,200,231)
+    print("3. Cookies prompt")
+    locate_element_10(384,1008,173,200,231)
+    hold(0.75,1.5)
 
 # Locate and click Log-in button   
 def login(email, password):
+    print("4. Logging In")
     locate_element(1730,36,13,103,172)
-    hold(1,2)
+    hold(5,7)
     y_locate_element(880,626,241,243,245)
     hold(0.5,0.75)
     y_locate_element(633,573,255,185,0)
-    hold(4,5)
-    pyautogui.press("f11")
-    hold(1,2)
-    pyautogui.write(email, interval=0.25)
-    hold(1,2)
-    pyautogui.press("enter")
     hold(5,7)
+    pyautogui.press("f11")
+    hold(5,7)
+    pyautogui.write(email, interval=0.25)
+    hold(0.5,0.75)
+    pyautogui.press("enter")
+    hold(7,10)
     pyautogui.write(password, interval=0.25)
     hold(0.5,0.75)
     pyautogui.press("enter")
-    hold(2,3)
+    hold(5,7)
     pyautogui.press("enter")
+    hold(10,15)
    
 # Configuring window 
 def conf_windows():
+    print("5. Configuring windows for editing")
     locate_element(395,708,241,243,245)
-    hold(4,6)
+    hold(15,19)
     locate_element(12,150,24,25,27)
     hold(1,2)
     locate_element(1895,84,255,255,255)
     hold(0.75,1)
+    hold(1,2)
     
+# Random choosing name from given file
+def random_name():
+    with open('name list.txt') as input_file:
+        long_list = [line.strip() for line in input_file]
+    random_line = random.choice(long_list)
+    return random_line
+
 # Edit the design     
-def edit_design():
+def edit_design(number):
+    print(f"Editing design #{number}")
     pyautogui.moveTo(1182,536,1, pyautogui.easeOutQuad)
     hold(0.35, 0.75)
     pyautogui.click()
@@ -214,25 +249,33 @@ def edit_design():
     hold(0.35, 0.75)
     pyautogui.hotkey('ctrl','a')
     hold(0.75,1)
-    pyautogui.write("Paijo", interval=0.15)
+    pyautogui.write(random_name(), interval=0.15)
     hold(0.35, 0.75)
     locate_element(1151,131,235,236,240)
+    hold(1,2)
     
 # Download the design
-def download():
+def download(number):
+    print(f"Downloading design #{number}")
     pyautogui.moveTo(1851,15,1, pyautogui.easeOutQuad)
     locate_element(1851,15,255,255,255)
     pyautogui.moveTo(1703,536,1, pyautogui.easeOutQuad)
     locate_element(1703,536,241,243,245)
     pyautogui.moveTo(1763,402,1, pyautogui.easeOutQuad)
     locate_element(1763,402,115,0,230)
-    hold(5,7)
+    hold(15,20)
     pyautogui.moveTo(1903,1052,1, pyautogui.easeOutQuad)
     pyautogui.click()
     hold(0.35, 0.75)
     locate_element(545,491,235,236,240)
     pyautogui.click()
-    hold(1,3)
+    hold(5,7)
+
+# Closing the chrome web browser
+def closechrome():
+    pyautogui.press("f11")
+    hold(5,7)
+    locate_element(1911,13,232,17,35)
 
 # Main Program
 def main():
@@ -242,29 +285,23 @@ def main():
     account_data = batch()
     df_log = log()
     print("************************************** Automation Start **************************************")
-    print(colored(text="To stop the program from running you can press 'ctrl' + 'q' on your keyboard.", color="red"))
+    print(colored(text="To stop the program from running you can press 'ctrl' + 'c' on your keyboard.", color="red"))
     for x in range(len(account_data)):
         date = time_stamp('date')
         start = time_stamp('time')
         account =  accounts(account_data)
         vpn(account['vpn'])
-        hold(10,15)
         openchrome()
-        hold(10,15)
         opencanva()
-        hold(7,9)
-        # allowcookies()
-        hold(0.75,1.5)
+        allowcookies()
         login(account['email'], account['password'])
-        hold(7,9)
         conf_windows()
-        hold(1,2)
         random_download = random.randint(75,150)
         for x in range(random_download):
-            edit_design()
-            hold(0.75,1)
-            download()
+            edit_design(x + 1)
+            download(x + 1 )
             hold(1,2)
+        closechrome()
         end = time_stamp('time')
         input_log(x, df_log, user, date, start, end, account['email'], random_download)
     df_name = f"Download_log_{str(date)}_user_{user}"
