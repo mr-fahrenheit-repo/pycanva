c08698eea1523b2273aaa35fae76a63bdf748abd
otong <work@rifkyashari.com>
2023-07-16 21:03:43 +0700

    Fixing anomaly error

--- main.py
+++ main.py
@@ -124,112 +124,158 @@ def vpn(code):
     os.system('"ipconfig /flushdns"')
     os.system(f'"mullvad relay set hostname {code}"')
     os.system('"mullvad connect"')
-    hold(10,12)
+    hold(10)
     os.system('"mullvad status"')
     print("===================================================")
-     
-# Locate element by x,y coordinate and RGB value
-def locate_element(x,y,r,g,b):
-    pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
-    while True:
+
+# Hold between two commands
+def hold(stop):
+    start = stop * 0.7
+    time.sleep(round(random.uniform(float(start), float(stop)),2))
+
+# Checking for element loaded
+def check_element(x_1, x_2, y, r, g, b):
+    for tries in range(50):
+        x_random = random.randint(x_1,x_2)
+        pyautogui.moveTo(x_random,y,1, pyautogui.easeOutQuad)
+        param = pyautogui.pixelMatchesColor(x_random, y, (r, g, b), tolerance=10)
+        if param == True:
+            break
+        else:
+            pass
+
+# Checking for element loaded
+def check_element_msft(x, y, r, g, b):
+    hold(2)
+    for tries in range(50):
+        xp = x + 10
+        y = y
+        pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
+        param_x = pyautogui.pixelMatchesColor(x, y, (r, g, b), tolerance=10)
+        param_xp = pyautogui.pixelMatchesColor(xp, y, (r, g, b), tolerance=10)
+        if param_x == True:
+            break
+        elif param_xp == True:
+            break
+        else:
+            y = y - 10
+            continue
+
+
+# Pick the login button      
+def pick_login(x,y,r,g,b):
+    y = y 
+    for tries in range(10):
+        pyautogui.moveTo(x,y,0.65, pyautogui.easeOutQuad)
+        hold(1)
         param = pyautogui.pixelMatchesColor(x, y, (r, g, b), tolerance=10)
         if param == True:
             pyautogui.click()
             break
+        else:
+            y = y - 57
+            
+            continue
 
-# Locate element by x,y coordinate and RGB value with timeout 10
-def locate_element_10(x,y,r,g,b):
-    pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
-    for x in range(5):
-        param = pyautogui.pixelMatchesColor(x, y, (r, g, b))
+# Locate element by x,y coordinate and RGB value
+def locate_element(x,y,r,g,b):
+    for i in range(5):
+        pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
+        param = pyautogui.pixelMatchesColor(x, y, (r, g, b), tolerance=50)
         if param == True:
             pyautogui.click()
             break
         else:
-            pyautogui.click()
-            time.sleep(2)
+            x_random = random.randint(100,1820)
+            y_random = random.randint(100,980)
+            pyautogui.moveTo(x_random,y_random,0.5, pyautogui.easeOutQuad)
             pass
 
 # Locate element by x,y coordinate and RGB value with y axis adjustment     
 def y_locate_element(x,y,r,g,b):
-    x = x
     y = y 
-    r = r
-    g = g
-    b = b
-    while True:
-        pyautogui.moveTo(x,y,1, pyautogui.easeOutQuad)
-        hold(1,1.5)
+    for tries in range(10):
+        pyautogui.moveTo(x,y,0.4, pyautogui.easeOutQuad)
         param = pyautogui.pixelMatchesColor(x, y, (r, g, b), tolerance=10)
         if param == True:
             pyautogui.click()
             break
         else:
-            y = y - 20
+            y = y - 10
             continue
-
-# Hold between two commands
-def hold(start, stop):
-	time.sleep(round(random.uniform(float(start), float(stop)),2))
 
 # Opening up Chrome browser
 def openchrome():
     print("1. Opening chrome browser")
     pyautogui.press('win')
-    hold(0.75,1)
-    pyautogui.write("chrome", interval = 0.12)
-    hold(0.75,1)
+    hold(1)
+    pyautogui.write("chrome", interval = 0.125)
+    hold(1)
     pyautogui.press("enter")
-    hold(10,15)
+    hold(6)
 
 # Open the target URL
 def opencanva():
     print("2. Opening Canva")
     pyautogui.write(url, interval=0.25)
-    hold(0.35,0.5)
+    hold(0.5)
     pyautogui.press("enter")
-    hold(2,3)
+    hold(1)
     pyautogui.press("f11")
-    hold(5,7)
-    
+    check_element(1795,1861,23,115,0,230)
+
 # Locate and click Accept Cookies prompt         
 def allowcookies():
     print("3. Cookies prompt")
-    locate_element_10(384,1008,173,200,231)
-    hold(0.75,1.5)
-
+    x_random = random.randint(312,445)
+    locate_element(x_random,1006,173,200,231)
+    
 # Locate and click Log-in button   
 def login(email, password):
     print("4. Logging In")
-    locate_element(1730,36,13,103,172)
-    hold(5,7)
-    y_locate_element(880,626,241,243,245)
-    hold(0.5,0.75)
+    x_random = random.randint(1700,1765)
+    locate_element(x_random,24,225,228,231)
+    hold(1)
+    check_element(849,915,405,225,228,231)
+    x_random = random.randint(849,915)
+    pick_login(x_random,644,242,243,245) 
+    hold(0.75)
     y_locate_element(633,573,255,185,0)
-    hold(5,7)
+    hold(1)
     pyautogui.press("f11")
-    hold(5,7)
+    check_element_msft(788,431,255,185,0)
+    # check_element(798,804,383,255,185,0)
+    hold(1)
     pyautogui.write(email, interval=0.25)
-    hold(0.5,0.75)
+    hold(0.75)
     pyautogui.press("enter")
-    hold(7,10)
+    hold(1)
+    check_element_msft(788,431,255,185,0)
     pyautogui.write(password, interval=0.25)
-    hold(0.5,0.75)
+    hold(0.75)
     pyautogui.press("enter")
-    hold(5,7)
-    pyautogui.press("enter")
-    hold(10,15)
-   
+    hold(1)
+    check_element_msft(788,431,255,185,0)
+    x_random_list = [random.randint(1032,1129),random.randint(921,1018)]
+    y_random = random.randint(628,649)
+    pyautogui.moveTo(random.choice(x_random_list,),y_random)
+    pyautogui.click()
+    hold(2)
+    check_element(1682,1807,18,115,0,230)
+    hold(5)
+
 # Configuring window 
 def conf_windows():
     print("5. Configuring windows for editing")
     locate_element(395,708,241,243,245)
-    hold(15,19)
-    locate_element(12,150,24,25,27)
-    hold(1,2)
+    check_element(5,12,588,24,25,27)
+    hold(2)
+    y_random_list = [589,518,444,376,295,234,150]
+    y_random_choice = random.choice(y_random_list)
+    locate_element(12,y_random_choice,24,25,27)
+    hold(1)
     locate_element(1895,84,255,255,255)
-    hold(0.75,1)
-    hold(1,2)
+    hold(1)
     
 # Random choosing name from given file
 def random_name():
@@ -242,40 +288,50 @@ def random_name():
 def edit_design(number):
     print(f"Editing design #{number}")
     pyautogui.moveTo(1182,536,1, pyautogui.easeOutQuad)
-    hold(0.35, 0.75)
+    hold(0.75)
     pyautogui.click()
-    hold(0.35, 0.75)
+    hold(0.75)
     pyautogui.click()
-    hold(0.35, 0.75)
+    hold(0.75)
     pyautogui.hotkey('ctrl','a')
-    hold(0.75,1)
+    hold(1)
     pyautogui.write(random_name(), interval=0.15)
-    hold(0.35, 0.75)
-    locate_element(1151,131,235,236,240)
-    hold(1,2)
-    
+    hold(0.75)
+    random_x = random.randint(641,1537)
+    locate_element(random_x,131,235,236,240)
+
+# Search for download button
+def search_download(number):
+    for x in range(number):
+        pyautogui.press('tab')
+        hold(0.5)
+    pyautogui.press('enter')
+
 # Download the design
 def download(number):
     print(f"Downloading design #{number}")
     pyautogui.moveTo(1851,15,1, pyautogui.easeOutQuad)
     locate_element(1851,15,255,255,255)
-    pyautogui.moveTo(1703,536,1, pyautogui.easeOutQuad)
-    locate_element(1703,536,241,243,245)
-    pyautogui.moveTo(1763,402,1, pyautogui.easeOutQuad)
-    locate_element(1763,402,115,0,230)
-    hold(15,20)
-    pyautogui.moveTo(1903,1052,1, pyautogui.easeOutQuad)
+    hold(2)
+    search_download(10)
+    hold(2)
+    search_download(7)
+    hold(13)
+    pyautogui.moveTo(1902,1051,1, pyautogui.easeOutQuad)
     pyautogui.click()
-    hold(0.35, 0.75)
-    locate_element(545,491,235,236,240)
+    hold(1)
+    x_random = random.randint(437,610)
+    y_random = random.randint(117,1006)
+    pyautogui.moveTo(x_random,y_random,1, pyautogui.easeOutQuad)
     pyautogui.click()
-    hold(5,7)
+    hold(1)
 
 # Closing the chrome web browser
 def closechrome():
     pyautogui.press("f11")
-    hold(5,7)
-    locate_element(1911,13,232,17,35)
+    hold(2)
+    pyautogui.moveTo(1897,12,1, pyautogui.easeOutQuad)
+    pyautogui.click()
 
 # Main Program
 def main():
@@ -296,11 +352,10 @@ def main():
         allowcookies()
         login(account['email'], account['password'])
         conf_windows()
-        random_download = random.randint(75,150)
-        for x in range(random_download):
-            edit_design(x + 1)
-            download(x + 1 )
-            hold(1,2)
+        random_download = random.randint(33,35)
+        for i in range(random_download):
+            edit_design(i + 1)
+            download(i + 1 )
         closechrome()
         end = time_stamp('time')
         input_log(x, df_log, user, date, start, end, account['email'], random_download)
